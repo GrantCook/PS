6e8a6011d2f319b3ccc93ee607e0d7fd58c347b2
Grant Cook <grantbcook@gmail.com>
2019-07-10 11:14:45 +1000

    Initial commin

---
+++ BethanysPieShop/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BethanysPieShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //Add Transient - new instance is returned everytime a IPieRespository is requested
+            services.AddTransient<IPieRepository, MockPieRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //Enable yellow screen of death
+            app.UseDeveloperExceptionPage();
+            //HTTP Status Codes
+            app.UseStatusCodePages();
+            //Search in wwwroot file and return static files
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
